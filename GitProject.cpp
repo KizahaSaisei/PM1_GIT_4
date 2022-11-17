00029738582444eff7c8e03fff2935a413a1f039
Kuzagane Kanzaki <kuzagane@gmail.com>
2022-11-17 11:43:04 +0900

    ローカルでコンフリクト変更

--- GitProject.cpp
+++ GitProject.cpp
@@ -5,9 +5,9 @@
 
 int main()
 {
-    std::cout << "Hello VisualStudio Git!\n";
+    std::cout << "Hello GitHub World!\n";
 
-    std::cout << "New branch changed!\n";
+    std::cout << "Clone changed!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
